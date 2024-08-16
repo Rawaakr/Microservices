a07c802a14753c1a5008d2380b0d6ba102574986
Rawaa <Rawaakr>
2024-08-16 15:03:55 +0100

    second commit

--- nats/src/subscriber/subscriber.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SubscriberController } from './subscriber.controller';
-import { SubscriberService } from './subscriber.service';
-
-@Module({
-  providers:[SubscriberService]
-  controllers: [SubscriberController]
-})
-export class SubscriberModule {}
